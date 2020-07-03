8e81c63d2009dfb8f4d2bc6b7382fbfb73bad74d
Colin Lees <colinleesa7x@gmail.com>
2020-07-03 08:32:59 +0100

    fix: amend file resolution to pick up on file name over extension

--- index.js
+++ index.js
@@ -6,112 +6,127 @@ const secret = process.env.SECRET
 const S3_URL = process.env.S3_URL
 
 const getEnvironment = string => {
-    const matches = string.match(/(int|qa|stage|prod){1}\//)
+  const matches = string.match(/(int|qa|stage|prod){1}\//)
 
-    if(!matches)
-        throw new Error('No environment provided.')
-    
-    return matches[1]
+  if (!matches) {
+    throw new Error('No environment provided.')
+  }
+
+  return matches[1]
 }
 
 const getVersionObject = string => {
-    let version = {
-        major: string.match(/([0-9]{1,})[.]{0,1}/),
-        minor: string.match(/[.]([0-9]{1,})[.]{0,1}/),
-        patch: string.match(/[0-9]{0,}[.][0-9]{0,}[.]([0-9]{1,})/),
-        original: string
-    }
+  const version = {
+    major: string.match(/([0-9]{1,})[.]{0,1}/),
+    minor: string.match(/[.]([0-9]{1,})[.]{0,1}/),
+    patch: string.match(/[0-9]{0,}[.][0-9]{0,}[.]([0-9]{1,})/),
+    original: string
+  }
 
-    version.major = version.major && version.major[1] ? version.major[1] : null
-    version.minor = version.minor && version.minor[1] ? version.minor[1] : null
-    version.patch = version.patch && version.patch[1] ? version.patch[1] : null
+  version.major = version.major && version.major[1] ? version.major[1] : null
+  version.minor = version.minor && version.minor[1] ? version.minor[1] : null
+  version.patch = version.patch && version.patch[1] ? version.patch[1] : null
 
-    return version
+  return version
 }
 
-const getS3Versions = () => new Promise((res, rej) => {
+const getS3Versions = () =>
+  new Promise((resolve, reject) => {
     const s3 = new AWS.S3({
-        region: 'eu-west-1',
-        credentials: new AWS.Credentials(key, secret)
+      region: 'eu-west-1',
+      credentials: new AWS.Credentials(key, secret)
     })
 
     const bucketParams = {
-        Bucket: 'stockportgov-design-system'
+      Bucket: 'stockportgov-design-system'
     }
 
     s3.listObjectsV2(bucketParams, (err, data) => {
-        if (err)
-            rej(new Error('No versions available.'))
-        else if (data)
-            res(data.Contents.map(_ => _.Key))
+      if (err) {
+        reject(new Error('No versions available.'))
+      } else if (data) {
+        resolve(data.Contents.map(_ => _.Key))
+      }
     })
-})
+  })
 
-/** int/1 */
 const getLatestVersion = async fileVersion => {
-    const versions = await getS3Versions()
+  const versions = await getS3Versions()
+  const environment = getEnvironment(fileVersion)
+  const versionSearchingFor = getVersionObject(fileVersion)
+  const filteredVersions = versions
+    .filter(version => version.includes(`${environment}/`))
+    .filter(version => /[aA-zZ]\/[0-9]+./.test(version))
 
-    const environment = getEnvironment(fileVersion) /** int */
-    const versionSearchingFor = getVersionObject(fileVersion) /* { major: 1, minor: null, patch: null, orignal: 'int/1.1.1' } */
-    const filteredVersions = versions
-        .filter(version => version.includes(`${environment}/`))
-        .filter(version => /[aA-zZ]\/[0-9]+./.test(version))
+  const foundVersion = semverSort
+    .desc(filteredVersions)
+    .map(version => getVersionObject(version))
+    .filter(
+      version =>
+        version.major === versionSearchingFor.major &&
+        (!versionSearchingFor.minor ||
+          version.minor === versionSearchingFor.minor) &&
+        (!versionSearchingFor.patch ||
+          version.patch === versionSearchingFor.patch)
+    )
 
-    const foundVersion = semverSort
-        .desc(filteredVersions)
-        .map(version => getVersionObject(version))
-        .filter(version =>
-            version.major === versionSearchingFor.major &&
-            (!versionSearchingFor.minor || version.minor === versionSearchingFor.minor) &&
-            (!versionSearchingFor.patch || version.patch === versionSearchingFor.patch))
+  if (!foundVersion.length) {
+    throw new Error('No versions available.')
+  }
 
-    if (!foundVersion.length)
-        throw new Error('No versions available.')
-
-    return foundVersion.map(v => v.original)
+  return foundVersion.map(v => v.original)
 }
 
 const versionHandler = async ({ path }) => {
-    if(!path)
-        return new Promise(res => res({
-            statusCode: 400
-        }))
-    
-        const matches = path.match(/^(\/(int|qa|stage|prod){1}\/?([0-9]+(\.[0-9]+){0,2})\/)([\w\W]*)(.min.css|.min.js){1}$/)
+  if (!path) {
+    return new Promise(resolve =>
+      resolve({
+        statusCode: 400
+      })
+    )
+  }
 
-    // wrapped in promise as lambda requries a promise to be returned
-    if (!matches)
-        return new Promise(res => res({
-            statusCode: 400
-        }))
+  const matches = path.match(
+    /^(\/(int|qa|stage|prod){1}\/?([0-9]+(\.[0-9]+){0,2})\/)([\w\W]*)(.min.css|.min.js){1}$/
+  )
 
-    const version = matches[1]
-    try {
-        const filesFound = await getLatestVersion(version)
-        const regex = new RegExp(`(${matches[matches.length-1]}){1}`, 'g')
-        const key = filesFound.find(key => !!regex.exec(key))
+  // wrapped in promise as lambda requries a promise to be returned
+  if (!matches) {
+    return new Promise(resolve =>
+      resolve({
+        statusCode: 400
+      })
+    )
+  }
 
-        if (!key)
-            throw new Error('No versions available.')
+  const version = matches[1]
+  try {
+    const filesFound = await getLatestVersion(version)
+    const regex = new RegExp(`(${matches[matches.length - 2]}){1}`, 'g')
+    const key = filesFound.find(key => !!regex.exec(key))
 
-        return {
-            statusCode: 302,
-            headers: {
-                Location: `${S3_URL}/${key}`
-            }
-        }
-    } catch (error) {
-        return {
-            statusCode: 404,
-            body: error.message
-        }
+    if (!key) {
+      throw new Error('No versions available.')
     }
+
+    return {
+      statusCode: 302,
+      headers: {
+        Location: `${S3_URL}/${key}`
+      }
+    }
+  } catch (error) {
+    return {
+      statusCode: 404,
+      body: error.message
+    }
+  }
 }
 
 module.exports = {
-    getEnvironment,
-    getVersionObject,
-    getS3Versions,
-    getLatestVersion,
-    handler: versionHandler
+  getEnvironment,
+  getVersionObject,
+  getS3Versions,
+  getLatestVersion,
+  handler: versionHandler
 }
--- index.test.js
+++ index.test.js
@@ -1,239 +1,279 @@
+/* eslint-env jest */
+
 const AWS = require('aws-sdk')
 const semverSort = require('semver-sort')
-let { getEnvironment, getVersionObject, getS3Versions, getLatestVersion, handler } = require('./index')
+const {
+  getEnvironment,
+  getVersionObject,
+  getS3Versions,
+  getLatestVersion,
+  handler
+} = require('./index')
 
 describe('getEnvironment', () => {
-    it('Should return int/qa/stage/prod environment', () => {
-        environment = [getEnvironment('int/'), getEnvironment('qa/'), getEnvironment('stage/'), getEnvironment('prod/')]
-        expect(environment[0]).toEqual('int')
-        expect(environment[1]).toEqual('qa')
-        expect(environment[2]).toEqual('stage')
-        expect(environment[3]).toEqual('prod')
-    })
+  it('Should return int/qa/stage/prod environment', () => {
+    const environment = [
+      getEnvironment('int/'),
+      getEnvironment('qa/'),
+      getEnvironment('stage/'),
+      getEnvironment('prod/')
+    ]
+    expect(environment[0]).toEqual('int')
+    expect(environment[1]).toEqual('qa')
+    expect(environment[2]).toEqual('stage')
+    expect(environment[3]).toEqual('prod')
+  })
 
-    it('Should throw error if it does not match the condition', () => {
-        expect(() => {
-            getEnvironment('test/')
-        }).toThrow()
+  it('Should throw error if it does not match the condition', () => {
+    expect(() => {
+      getEnvironment('test/')
+    }).toThrow()
 
-        expect(() => {
-            getEnvironment('test')
-        }).toThrow()
+    expect(() => {
+      getEnvironment('test')
+    }).toThrow()
 
-        expect(() => {
-            getEnvironment('int')
-        }).toThrow()
-    })
+    expect(() => {
+      getEnvironment('int')
+    }).toThrow()
+  })
 })
 
 describe('getVersionObject', () => {
-    it('Should return correct version', () => {
-        version = getVersionObject('/1.2.3/')
-        expect(version.major).toBe('1')
-        expect(version.minor).toBe('2')
-        expect(version.patch).toBe('3')
-        expect(version.original).toBe('/1.2.3/')
-    })
+  it('Should return correct version', () => {
+    const version = getVersionObject('/1.2.3/')
+    expect(version.major).toBe('1')
+    expect(version.minor).toBe('2')
+    expect(version.patch).toBe('3')
+    expect(version.original).toBe('/1.2.3/')
+  })
 
-    it('Should return correct version if only major provided', () => {
-        version = getVersionObject('/1/')
-        expect(version.major).toBe('1')
-        expect(version.minor).toBe(null)
-        expect(version.patch).toBe(null)
-        expect(version.original).toBe('/1/')
-    })
+  it('Should return correct version if only major provided', () => {
+    const version = getVersionObject('/1/')
+    expect(version.major).toBe('1')
+    expect(version.minor).toBe(null)
+    expect(version.patch).toBe(null)
+    expect(version.original).toBe('/1/')
+  })
 
-    it('Should return correct version if only major and minor provided', () => {
-        version = getVersionObject('/1.2/')
-        expect(version.major).toBe('1')
-        expect(version.minor).toBe('2')
-        expect(version.patch).toBe(null)
-        expect(version.original).toBe('/1.2/')
-    })
+  it('Should return correct version if only major and minor provided', () => {
+    const version = getVersionObject('/1.2/')
+    expect(version.major).toBe('1')
+    expect(version.minor).toBe('2')
+    expect(version.patch).toBe(null)
+    expect(version.original).toBe('/1.2/')
+  })
 
-    it('Should return version if version provided does not match condition', () => {
-        version = getVersionObject('/test/')
-        expect(version.major).toBe(null)
-        expect(version.minor).toBe(null)
-        expect(version.patch).toBe(null)
-        expect(version.original).toBe('/test/')
-    })
+  it('Should return version if version provided does not match condition', () => {
+    const version = getVersionObject('/test/')
+    expect(version.major).toBe(null)
+    expect(version.minor).toBe(null)
+    expect(version.patch).toBe(null)
+    expect(version.original).toBe('/test/')
+  })
 })
 
 describe('getS3Versions', () => {
-    it('Should return list of keys', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [{
-                    Key: '12345TEST'
-                }]
-            })
-        })
-
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
-
-        AWS.Credentials = jest.fn()
-
-        const result = await getS3Versions()
-
-        expect(result).toContain('12345TEST')
-        expect(result.length).toBe(1)
-        expect(mockListObjectV2.mock.calls.length).toBe(1)
-        expect(AWS.Credentials.mock.calls.length).toBe(1)
+  it('Should return list of keys', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          {
+            Key: '12345TEST'
+          }
+        ]
+      })
     })
 
-    it('Should throw error', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(1, undefined)
-        })
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
 
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
+    AWS.Credentials = jest.fn()
 
-        AWS.Credentials = jest.fn()
+    const result = await getS3Versions()
 
-        try {
-            await getS3Versions()
-        } catch (error) {
-            expect(error.message).toEqual('No versions available.')
-        }
-        
-        expect(mockListObjectV2.mock.calls.length).toBe(1)
-        expect(AWS.Credentials.mock.calls.length).toBe(1)
+    expect(result).toContain('12345TEST')
+    expect(result.length).toBe(1)
+    expect(mockListObjectV2.mock.calls.length).toBe(1)
+    expect(AWS.Credentials.mock.calls.length).toBe(1)
+  })
+
+  it('Should throw error', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(1, undefined) // eslint-disable-line
     })
-    
-    afterEach(() => jest.resetAllMocks())
+
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
+
+    AWS.Credentials = jest.fn()
+
+    try {
+      await getS3Versions()
+    } catch (error) {
+      expect(error.message).toEqual('No versions available.')
+    }
+
+    expect(mockListObjectV2.mock.calls.length).toBe(1)
+    expect(AWS.Credentials.mock.calls.length).toBe(1)
+  })
+
+  afterEach(() => jest.resetAllMocks())
 })
 
 describe('getLatestVersion', () => {
-    beforeEach(() => {
-        AWS.Credentials = jest.fn()
-        semverSort.desc = jest.fn(versions => versions)
+  beforeEach(() => {
+    AWS.Credentials = jest.fn()
+    semverSort.desc = jest.fn(versions => versions)
+  })
+
+  it('Should find specific version', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          { Key: 'int/2.0.1' },
+          { Key: 'int/1.1.2' },
+          { Key: 'int/1.1.1' }
+        ]
+      })
     })
 
-    it('Should find specific version', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [{Key: 'int/2.0.1'}, {Key: 'int/1.1.2'}, {Key: 'int/1.1.1'}]
-            })
-        })
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
 
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
+    const result = await getLatestVersion('int/1.1.1')
 
-        const result = await getLatestVersion('int/1.1.1')
+    expect(result.length).toBe(1)
+    expect(result).toContain('int/1.1.1')
+  })
 
-        expect(result.length).toBe(1)
-        expect(result).toContain('int/1.1.1')
+  it('Should find latest version of major provided', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          { Key: 'int/2.0.1' },
+          { Key: 'int/1.1.2' },
+          { Key: 'int/1.1.1' }
+        ]
+      })
     })
 
-    it('Should find latest version of major provided', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [{Key: 'int/2.0.1'}, {Key: 'int/1.1.2'}, {Key: 'int/1.1.1'}]
-            })
-        })
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
 
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
-        
-        const result = await getLatestVersion('int/1')
+    const result = await getLatestVersion('int/1')
 
-        expect(result.length).toBe(2)
-        expect(result[0]).toEqual('int/1.1.2')
+    expect(result.length).toBe(2)
+    expect(result[0]).toEqual('int/1.1.2')
+  })
+
+  it('Should find latest version of major.minor provided', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          { Key: 'int/2.0.1' },
+          { Key: 'int/1.1.2' },
+          { Key: 'int/1.1.1' }
+        ]
+      })
     })
 
-    it('Should find latest version of major.minor provided', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [{Key: 'int/2.0.1'}, {Key: 'int/1.1.2'}, {Key: 'int/1.1.1'}]
-            })
-        })
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
 
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
-        
-        const result = await getLatestVersion('int/1.1')
+    const result = await getLatestVersion('int/1.1')
 
-        expect(result.length).toBe(2)
-        expect(result[0]).toEqual('int/1.1.2')
+    expect(result.length).toBe(2)
+    expect(result[0]).toEqual('int/1.1.2')
+  })
+
+  it('Should throw error', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          { Key: 'int/2.0.1' },
+          { Key: 'int/1.1.2' },
+          { Key: 'int/1.1.1' }
+        ]
+      })
     })
 
-    it('Should throw error', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [{Key: 'int/2.0.1'}, {Key: 'int/1.1.2'}, {Key: 'int/1.1.1'}]
-            })
-        })
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
 
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
-        
-        try {
-            await getLatestVersion('int/2.3.5')
-        } catch (error) {
-            expect(error.message).toEqual('No versions available.')
-        }
-    })
+    try {
+      await getLatestVersion('int/2.3.5')
+    } catch (error) {
+      expect(error.message).toEqual('No versions available.')
+    }
+  })
 
-    afterEach(() => jest.resetAllMocks())
+  afterEach(() => jest.resetAllMocks())
 })
 
 describe('versionHandler', () => {
-    beforeEach(() => {
-        AWS.Credentials = jest.fn()
-        semverSort.desc = jest.fn(versions => versions)
+  beforeEach(() => {
+    AWS.Credentials = jest.fn()
+    semverSort.desc = jest.fn(versions => versions)
+  })
+
+  it('Should return 400 when path null', async () => {
+    expect((await handler({})).statusCode).toBe(400)
+  })
+
+  it('Should return 400 when path incorrect format', async () => {
+    expect((await handler({ path: 'invalid_path' })).statusCode).toBe(400)
+  })
+
+  it('Should return 404 when getLatestVersion throws error', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          { Key: 'int/2.0.1' },
+          { Key: 'int/1.1.2' },
+          { Key: 'int/1.1.1' }
+        ]
+      })
     })
 
-    it('Should return 400 when path null', async () => {
-        expect((await handler({})).statusCode).toBe(400)
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
+
+    const result = await handler({ path: '/int/1.1.0/smbc-frontend.min.css' })
+    expect(result.statusCode).toBe(404)
+    expect(result.body).toEqual('No versions available.')
+  })
+
+  it('Should return 302 when file found', async () => {
+    const mockListObjectV2 = jest.fn((bucketParams, callback) => {
+      callback(undefined, {
+        Contents: [
+          { Key: 'int/2.0.1/smbc-frontend-ie8.min.css' },
+          { Key: 'int/1.1.2/smbc-frontend-ie8.min.css' },
+          { Key: 'int/1.1.1/smbc-frontend-ie8.min.css' }
+        ]
+      })
     })
 
-    it('Should return 400 when path incorrect format', async () => {
-        expect((await handler({path: 'invalid_path'})).statusCode).toBe(400)
+    AWS.S3 = jest.fn().mockImplementation(() => ({
+      listObjectsV2: mockListObjectV2
+    }))
+
+    const result = await handler({
+      path: '/int/1.1.1/smbc-frontend-ie8.min.css'
     })
+    expect(result.statusCode).toBe(302)
+    expect(result.headers.Location).toContain(
+      '/int/1.1.1/smbc-frontend-ie8.min.css'
+    )
+  })
 
-    it('Should return 404 when getLatestVersion throws error', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [{Key: 'int/2.0.1'}, {Key: 'int/1.1.2'}, {Key: 'int/1.1.1'}]
-            })
-        })
-
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
-        
-        const result = await handler({path: '/int/1.1.0/smbc-frontend.min.css'})
-        expect(result.statusCode).toBe(404)
-        expect(result.body).toEqual('No versions available.')
-    })
-
-    it('Should return 302 when file found', async () => {
-        const mockListObjectV2 = jest.fn((bucketParams, callback) => {
-            callback(undefined, {
-                Contents: [
-                    {Key: 'int/2.0.1/smbc-frontend-ie8.min.css'},
-                    {Key: 'int/1.1.2/smbc-frontend-ie8.min.css'},
-                    {Key: 'int/1.1.1/smbc-frontend-ie8.min.css'}]
-            })
-        })
-
-        AWS.S3 = jest.fn().mockImplementation(() => ({
-            listObjectsV2: mockListObjectV2
-        }))
-        
-        const result = await handler({path: '/int/1.1.1/smbc-frontend-ie8.min.css'})
-        expect(result.statusCode).toBe(302)
-        expect(result.headers.Location).toContain('/int/1.1.1/smbc-frontend-ie8.min.css')
-    })
-
-    afterEach(() => jest.resetAllMocks())
+  afterEach(() => jest.resetAllMocks())
 })
